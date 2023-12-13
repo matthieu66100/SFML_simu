861bb7632c4a515bd224de20fab487b646b147bc
Matthieu Basto <matthieu.basto@gmail.com>
2023-12-13 17:33:16 +0100

    map planets informations

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 using namespace sf;
 
-struct size{
-    double sun      = 1391400;
-    double earth    = 12756;
-    double mars     = 6792;
-    double venus    = 12104;
-    double uranus   = 51118;
-    double jupiter  = 142984;
-    double mercure  = 4879;
-    double saturne  = 120536;
-    double neptune  = 49528;
 
+unordered_map<string, float> planetSize
+{
+    {"sun"      ,1391400/10000},
+    {"jupiter"  ,142984/1000},
+    {"saturne"  ,120536/1000},
+    {"uranus"   ,51118/1000},
+    {"neptune"  ,49528/1000},
+    {"earth"    ,12756/1000},
+    {"venus"    ,12104/1000},
+    {"mars"     ,6792/1000},
+    {"mercure"  ,487/10009}
 };
-struct distance_from_sun{
-    double earth    = 149600000;
-    double mars     = 227900000;
-    double venus    = 108200000;
-    double uranus   = 2872500000;
-    double jupiter  = 778600000;
-    double mercure  = 57900000;
-    double saturne  = 1433500000;
-    double neptune  = 4495100000;
-
+unordered_map<string, int> planetDistance{
+    {"neptune",   4495100000},
+    {"uranus",    2872500000},
+    {"saturne",   1433500000},
+    {"jupiter",   778600000},
+    {"mars",      227900000},
+    {"earth",     149600000},
+    {"venus",     108200000},
+    {"mercure",   57900000}
 };
 
 class CelestialBody
@@ -105,15 +105,15 @@ int main(){
 
     SolarSystemSim simulator(1500, 1000);
 
-    CelestialBody sun(100.f, Vector2f(1500/2,1000/2), Vector2f(0,0), Color::Yellow);
-    CelestialBody earth(10.f, Vector2f(400,300), Vector2f(0,0), Color::Blue);
-    CelestialBody mars(5.f, Vector2f(800,300), Vector2f(0,0), Color::Red);
-    CelestialBody venus(5.f, Vector2f(800,300), Vector2f(0,0), Color::Red);
-    CelestialBody uranus(5.f, Vector2f(800,300), Vector2f(0,0), Color::Red);
-    CelestialBody jupiter(5.f, Vector2f(800,300), Vector2f(0,0), Color::Red);
-    CelestialBody mercure(5.f, Vector2f(800,300), Vector2f(0,0), Color::Red);
-    CelestialBody saturne(5.f, Vector2f(800,300), Vector2f(0,0), Color::Red);
-    CelestialBody neptune(5.f, Vector2f(800,300), Vector2f(0,0), Color::Red);
+    CelestialBody sun(planetSize["sun"], Vector2f(1500/2,1000/2), Vector2f(0,0), Color::Yellow);
+    CelestialBody earth(planetSize["earth"], Vector2f(400,300), Vector2f(0,0), Color::Blue);
+    CelestialBody mars(planetSize["mars"], Vector2f(800,300), Vector2f(0,0), Color::Red);
+    CelestialBody venus(planetSize["venus"], Vector2f(800,300), Vector2f(0,0), Color::Red);
+    CelestialBody uranus(planetSize["uranus"], Vector2f(800,300), Vector2f(0,0), Color::Red);
+    CelestialBody jupiter(planetSize["jupiter"], Vector2f(800,300), Vector2f(0,0), Color::Red);
+    CelestialBody mercure(planetSize["mercure"], Vector2f(800,300), Vector2f(0,0), Color::Red);
+    CelestialBody saturne(planetSize["saturne"], Vector2f(800,300), Vector2f(0,0), Color::Red);
+    CelestialBody neptune(planetSize["neptune"], Vector2f(800,300), Vector2f(0,0), Color::Red);
 
     simulator.addCelestialBody(sun);
     simulator.addCelestialBody(earth);
